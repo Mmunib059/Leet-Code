8cb169be98bc1f4ebaa855d06d43b17213d75b77
Muhammad Muneeb <rmunib604@gmail.com>
2025-01-25 23:42:41 +0500

    a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.

---
+++ A3.js
@@ -0,0 +1,30 @@
+// Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+// toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+// notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
+
+function expect(val) {
+    return {
+        toBe: function (otherVal) {
+            if (val !== otherVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function (otherVal) {
+            if (val === otherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+}
+// Example usage:
+expect(5).toBe(5); // Returns true
+expect(5).notToBe(3); // Returns true
+
+// These will throw errors:
+// expect(5).toBe(3); // Throws "Not Equal"
+// expect(5).notToBe(5); // Throws "Equal"
