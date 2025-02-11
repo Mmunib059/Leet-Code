8b465edec5d81d2b90b79e73c66bd8fceb7985cb
Muhammad Muneeb <rmunib604@gmail.com>
2025-02-11 23:43:31 +0500

    example

--- A9.js
+++ A9.js
@@ -3,4 +3,12 @@
 function argumentsLength() {
     return arguments.length;
   }
+  // ? Should return 0
+  console.log(argumentsLength());
+  // ? Should return 1
+  console.log(argumentsLength(1));
+  // ? Should return 2
+  console.log(argumentsLength(1, 2));
+  // ? Should return 3
+  console.log(argumentsLength(1, 2, 3));
   
