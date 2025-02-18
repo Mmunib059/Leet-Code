ac7f469f564f73273052eeb41507f997f209f899
Muhammad Muneeb <rmunib604@gmail.com>
2025-02-18 23:45:00 +0500

    example

--- A1.js
+++ A1.js
@@ -10,3 +10,16 @@ function createHelloWorld() {
 // Example usage:
 const hello = createHelloWorld();
 console.log(hello()); // Output: "Hello World"
+
+// Path: Leet-Code/A5.js
+// Create Counter Function
+// Write a function createCounter. It should return a new function that returns a unique number every time it is called.
+
+function createCounter() {
+    let count = 0;
+
+    return function() {
+        return count++;
+    };
+}
+
