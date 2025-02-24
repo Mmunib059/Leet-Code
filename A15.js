e3245a34e40a44c0cfe0cff0c62119cd04c58d53
Muhammad Muneeb <rmunib604@gmail.com>
2025-02-24 23:24:18 +0500

    example

--- A15.js
+++ A15.js
@@ -3,4 +3,18 @@
 
 async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
+
 }
+
+// Example 1
+// Input: 1000
+// Output: (After 1 second)
+// Explanation: The function sleeps for 1 second.
+// sleep(1000).then(() => console.log('Done'));
+// Output: Done
+// Example 2
+// Input: 2000
+// Output: (After 2 seconds)
+// Explanation: The function sleeps for 2 seconds.
+// sleep(2000).then(() => console.log('Done'));
+// Output: Done
