d72ac21be300be185d389ef6011126ee8f68c5dd
Muhammad Muneeb <rmunib604@gmail.com>
2025-03-05 23:44:21 +0500

    example

--- A13.js
+++ A13.js
@@ -5,11 +5,21 @@ const mappedArray = array.map(num => num * 2);
 console.log(mappedArray); // Output: [2, 4, 6, 8, 10]
 
 // test case
+const callCount1 = 0;
+const memoizedFn1 = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount1); // 1
+
+// test case
 const callCount = 0;
 const memoizedFn = memoize(function (a, b) {
     callCount += 1;
     return a + b;
 });
 memoizedFn(2, 3); // 5
-memoizedFn(2, 3); // 5
-console.log(callCount); // 1
+memoizedFn(3, 3); // 6
+console.log(callCount); // 2
